70ab161654b132b5dede06d67dcee2e54a36eb58
kevin otieno <kelvinotix@gmail.com>
2023-02-16 23:08:26 +0300

    updated

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,5 @@
-import { configureStore ,applyMiddleware} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../Features/Auth/AuthSlice'
-import {composeWithDevTools} from 'redux-devtools-extension'
 export const store = configureStore({
   reducer: {
     auth:authReducer
