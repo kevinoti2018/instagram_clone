fbc8794ca08dc6397636821730648964b4ac876d
Kevin Otieno <69906915+kevinoti2018@users.noreply.github.com>
2023-06-23 10:46:13 +0300

    Update user model
    removed redundant code

--- server/models/User.js
+++ server/models/User.js
@@ -29,19 +29,7 @@ const userSchema = new mongoose.Schema({
     expireToken:String
 },{timestamps:true})
 
-    // userSchema.pre('save', async function(next){
-    //     if(!this.isModified('password')){
-    //         next();
-    //     }
-    
-    //     const salt = await bcrypt.genSalt(10)
-    //     this.password= await bcrypt.hash(this.password,salt);
-    // })
 
-    // userSchema.methods.matchPassword = async function (enteredPassword) {
-    //     return await bcrypt.compare(enteredPassword, this.password);
-    //   };
-    
 
 const User = mongoose.model('User',userSchema)
 
